9a19154a634d9624ceeb3a67874e8e9562d1ec3c
Josh Burne <joshb.contact@gmail.com>
2025-06-25 12:09:00 +0100

    final backup before modifying files with main()

--- src/main.py
+++ src/main.py
@@ -1,7 +1,53 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+
+    
+
+
+def verify_directory_path(directory):
+    # returns true if the directory exists, false otherwise
+    return os.path.exists(directory)
+
+def list_directory_files(directory):
+    # returns a list of all the files in a directory
+    return os.listdir(directory)
+
+def found_file_at_path(path_to_file):
+    # returns true is the path_to_file is an existing file.
+    return os.path.isfile(path_to_file)
+
+def create_new_directory(path_ending_with_name):
+    # creates a new directory, using a path and ending with the /directory_name
+    return os.mkdir(path_ending_with_name)
+
+def copy_file(old, new):
+    return shutil.copy(old, new)
+
+def delete_directory(path):
+    return shutil.rmtree(path)
+
+
+
 def main():
-    test_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(test_node)
+    # file directory locations.
+    root_dir = "/home/joshburne/workspace/boot.dev/static_site_generator"
+    src_dir = f"{root_dir}/src"
+    public_dir = f"{root_dir}/public"
+    static_dir = f"{root_dir}/static"
+    test_dir = f"{root_dir}/testdir"
+    source_dir = f"{root_dir}/sourcedir"
+
+    create_new_directory(source_dir)
+
+    delete_directory(test_dir)
+
+    
+
+
+
 
 
 main()
+
